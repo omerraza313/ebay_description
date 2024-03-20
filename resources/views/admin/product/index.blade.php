3338ec8d87453708a0fbd067de7d7167806194ee
omerraza313 <omerraza313@gmail.com>
2024-03-20 22:03:34 +0500

    All code moved to git

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,5 +1,11 @@
 @extends('admin.layouts.master')
 @section('products_select', 'active')
+@push('custom-css')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+@endpush
 <style>
     .pagination li a {
         font-size: 14px; /* Adjust the font size as needed */
@@ -10,6 +16,10 @@
       max-height: 20px;
     }
     nav[role="navigation"] > div:first-child {
+      display: none;
+    }
+/*    css for hiding the dataTables pagination*/
+    #example1_info{
       display: none;
     }
 </style>
@@ -77,7 +87,7 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                <table class="table table-bordered table-striped">
+                <table id="example1" class="table table-bordered table-striped">
                   <thead>
                   <tr>
                     <th style="width: 10%;">Sr.No #</th>
@@ -187,6 +197,37 @@
     });
 </script>
 
-
+@push('custom-js')
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('plugins/jszip/jszip.min.js')}}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+    <script>
+      $(function () {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "pageLength": 30,
+            "paging": false,
+            "searching": true,
+            "search": {"caseInsensitive": true},
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
+            "columnDefs": [
+                { "searchable": true, "targets": [0, 1] }
+            ]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      });
+    </script>
+@endpush
 
 @endsection
